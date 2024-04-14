22bd9068c706e31902c9083571cd7160bd9ee4ec
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-04-14 16:24:31 -0700

    Created message.java

---
+++ message.java
@@ -0,0 +1,37 @@
+import java.sql.Timestamp;
+import java.time.Instant;
+public class message {
+    private String sender;
+    private String recipient;
+    Timestamp instant;
+    private String messageContent;
+
+    public message(String sender, String recipient, String messageContent)
+    {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.messageContent = messageContent;
+        instant = Timestamp.from(Instant.now());
+
+    }
+
+    public String getSender()
+    {
+        return sender;
+    }
+
+    public String getRecipient()
+    {
+        return recipient;
+    }
+
+    public Timestamp getTimestamp()
+    {
+        return instant;
+    }
+    
+    public String getMessageContent()
+    {
+        return messageContent;
+    }
+}
