7abc6d1201f96179dbc08f1f9f549d6f7c3cc055
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-04-14 18:24:19 -0700

    Updated send() function

--- user.java
+++ user.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class user {
 
     ArrayList<String> inbox = new ArrayList<String>();
 
-    public void send(user[] recipient, message note)
+    public message send(user[] recipient, message note)
     {
+        Scanner messageContent = new Scanner(System.in);
         System.out.println("Message being sent to " + recipient);
+        return note;
     }
 
     public void recieve(user sender, message note)
