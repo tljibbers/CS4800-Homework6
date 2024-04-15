660236b28b995e63399f69b8d324855dafecea9c
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-04-15 11:55:35 -0700

    Added ChatLog feature

--- user.java
+++ user.java
@@ -5,6 +5,7 @@ public class user {
     Boolean isRegistered;
     Boolean isBlocked;
     ChatServer chat = new ChatServer();
+    ChatHistory history = new ChatHistory();
     ArrayList<String> inbox = new ArrayList<String>();
     message note;
     private MessageMemento memento = new MessageMemento(note);
@@ -12,6 +13,7 @@ public class user {
     public void send(user user, user[] recipients, message note)
     {
         chat.send(user, recipients, note);
+        history.addMessage(user, note);
     }
 
     public void recieve(user sender, message note)
