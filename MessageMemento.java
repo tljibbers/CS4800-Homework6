0653054a307be2d00e019efbb5ba6c2177fd1f0c
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-04-14 18:14:36 -0700

    Made MessageMemento

--- MessageMemento.java
+++ MessageMemento.java
@@ -1,3 +1,16 @@
 public class MessageMemento {
+    private String state;
+
+    public MessageMemento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
     
 }
