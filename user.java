6cc3a376c72fb3cd6cea9c4f9443a1e60ba2c418
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-04-14 17:51:01 -0700

    Created user

--- user.java
+++ user.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class user {
+
+    ArrayList<message> inbox = new ArrayList<message>();
+
+    public void send(user[] recipient, message note)
+    {
+        System.out.println("Message being sent to " + recipient);
+    }
+
+    public void recieve(user sender, message note2)
+    {
+        System.out.println("Message being recieved from " + sender);
+        inbox.add(note2);
+    }
     
 }
