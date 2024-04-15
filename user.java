e0dafdf375f320530e1ec78f3698b8b64cbec3b9
tljibbers <84957009+tljibbers@users.noreply.github.com>
2024-04-14 18:09:43 -0700

    Edited Files

--- user.java
+++ user.java
@@ -2,17 +2,17 @@ import java.util.ArrayList;
 
 public class user {
 
-    ArrayList<message> inbox = new ArrayList<message>();
+    ArrayList<String> inbox = new ArrayList<String>();
 
     public void send(user[] recipient, message note)
     {
         System.out.println("Message being sent to " + recipient);
     }
 
-    public void recieve(user sender, message note2)
+    public void recieve(user sender, message note)
     {
         System.out.println("Message being recieved from " + sender);
-        inbox.add(note2);
+        inbox.add(note.getMessageContent());
     }
     
 }
